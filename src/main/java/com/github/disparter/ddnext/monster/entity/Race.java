617a56a8f567d543992aefed9d3a5873fbbf8211
Felipe Faria <felipe.faria@ifactory.com.br>
2018-05-04 11:39:23 -0300

    Filled with monster's races

--- src/main/java/com/github/disparter/ddnext/monster/entity/Race.java
+++ src/main/java/com/github/disparter/ddnext/monster/entity/Race.java
@@ -1,5 +1,17 @@
 package com.github.disparter.ddnext.monster.entity;
 
 public enum Race {
-	Celestial
+	Aberration,
+	Beast,
+	Celestial,
+	Construct,
+	Dragon,
+	Elemental,
+	Fiend,
+	Giant,
+	Humanoid,
+	Monstrosity,
+	Ooze,
+	Plant,
+	Undead
 }
