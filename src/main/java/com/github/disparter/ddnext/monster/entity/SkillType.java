136aee92af91d729907284ff03ab844bca2467f2
Felipe Faria <felipe.faria@ifactory.com.br>
2018-05-04 11:32:47 -0300

    Filled with skill types

--- src/main/java/com/github/disparter/ddnext/monster/entity/SkillType.java
+++ src/main/java/com/github/disparter/ddnext/monster/entity/SkillType.java
@@ -1,5 +1,31 @@
 package com.github.disparter.ddnext.monster.entity;
 
 public enum SkillType {
-	Perception
+	//Strength Based
+	Athletics,
+	
+	//Dexterity Based
+	Acrobatics,
+	SleightOfHand,
+	Stealth,
+	
+	//Intelligence Based
+	Arcana,
+	History,
+	Investigation,
+	Nature,
+	Religion,
+	
+	//Wisdom Based
+	AnimalHandling,
+	Insight,
+	Medicine,
+	Perception,
+	Survival,
+	
+	//Charisma Based
+	Deception,
+	Intimidation,
+	Performance,
+	Persuasion
 }
