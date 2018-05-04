25cab3ed3809f8eca5056741c600f7345f2965be
Felipe Faria <felipe.faria@ifactory.com.br>
2018-05-04 09:20:19 -0300

    Project started with SpringBoot configurations

---
+++ src/main/java/com/github/disparter/ddnext/monster/MonsterApplication.java
@@ -0,0 +1,12 @@
+package com.github.disparter.ddnext.monster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/disparter/ddnext/monster/MonsterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.disparter.ddnext.monster;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MonsterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
