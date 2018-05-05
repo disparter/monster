398f0231a80dd8feb8f484d0130c9b3444a77657
Felipe Faria <felipe.faria@ifactory.com.br>
2018-05-05 09:20:32 -0300

    Filled with condition immunities

--- src/main/java/com/github/disparter/ddnext/monster/entity/ConditionImmunityType.java
+++ src/main/java/com/github/disparter/ddnext/monster/entity/ConditionImmunityType.java
@@ -1,5 +1,25 @@
 package com.github.disparter.ddnext.monster.entity;
 
 public enum ConditionImmunityType {
-	Charmed
+	//Common
+	Blinded,
+	Charmed,
+	Deafened,
+	Fatigued,
+	Frightned,
+	Grappled,
+	Paralyzed,
+	Petrified,
+	Poisoned,
+	Prone,
+	Restrained,
+	Stunned,
+	Unconscious,
+	
+	//Incommon
+	Exhaustion,
+	Surprised,
+	Invisible,
+	Incapacitated
+	
 }
