b11ad8393c466185e9d13be6f9d70cd6a6e37a7f
Felipe Faria <felipe.a.b.faria@gmail.com>
2018-05-10 19:31:30 -0300

    Adjusted cors configuration

--- src/main/java/com/github/disparter/ddnext/monster/MonsterApplication.java
+++ src/main/java/com/github/disparter/ddnext/monster/MonsterApplication.java
@@ -2,10 +2,6 @@ package com.github.disparter.ddnext.monster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SuppressWarnings("deprecation")
 @SpringBootApplication
@@ -15,13 +11,4 @@ public class MonsterApplication {
 		SpringApplication.run(MonsterApplication.class, args);
 	}
 	
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/").allowedOrigins("http://localhost:8100", "http://dm-shield.s3-website-sa-east-1.amazonaws.com");
-            }
-        };
-    }
 }
