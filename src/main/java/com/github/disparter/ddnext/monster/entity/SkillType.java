ebe3398342118a5f465978072ebff59c0f90df73
Felipe Faria <felipe.a.b.faria@gmail.com>
2018-05-17 16:03:02 -0300

    Added the keys for skill types

--- src/main/java/com/github/disparter/ddnext/monster/entity/SkillType.java
+++ src/main/java/com/github/disparter/ddnext/monster/entity/SkillType.java
@@ -1,31 +1,53 @@
 package com.github.disparter.ddnext.monster.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.github.disparter.ddnext.monster.helper.Translator;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum SkillType {
-	//Strength Based
-	Athletics,
 	
-	//Dexterity Based
-	Acrobatics,
-	SleightOfHand,
-	Stealth,
+	//strength Based
+	Athletics("skill.athletics.name","skill.athletics.description"),
 	
-	//Intelligence Based
-	Arcana,
-	History,
-	Investigation,
-	Nature,
-	Religion,
+	//dexterity based
+	Acrobatics("skill.acrobatics.name","skill.acrobatics.description"),
+	SleightOfHand("skill.sleightofhand.name","skill.sleightofhand.description"),
+	Stealth("skill.stealth.name","skill.stealth.description"),
 	
-	//Wisdom Based
-	AnimalHandling,
-	Insight,
-	Medicine,
-	Perception,
-	Survival,
+	//intelligence based
+	Arcana("skill.arcana.name","skill.arcana.description"),
+	History("skill.history.name","skill.history.description"),
+	Investigation("skill.investigation.name","skill.investigation.description"),
+	Nature("skill.nature.name","skill.nature.description"),
+	Religion("skill.religion.name","skill.religion.description"),
 	
-	//Charisma Based
-	Deception,
-	Intimidation,
-	Performance,
-	Persuasion
+	//wisdom based
+	AnimalHandling("skill.animalhandling.name","skill.animalhandling.description"),
+	Insight("skill.insight.name","skill.insight.description"),
+	Medicine("skill.medicine.name","skill.medicine.description"),
+	Perception("skill.perception.name","skill.perception.description"),
+	Survival("skill.survival.name","skill.survival.description"),
+	
+	//charisma based
+	Deception("skill.deception.name","skill.deception.description"),
+	Intimidation("skill.intimidation.name","skill.intimidation.description"),
+	Performance("skill.performance.name","skill.performance.description"),
+	Persuasion("skill.persuasion.name","skill.persuasion.description");
+
+	private String name;
+	private String description;
+	
+	SkillType(String name, String description){
+		this.name = name;
+		this.description = description;
+	}
+	
+	public String getName() {
+		return Translator.get(this.name);
+	}
+
+	public String getDescription() {
+		return Translator.get(this.description);
+	}
+	
 }
